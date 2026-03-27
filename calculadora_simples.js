0376f1c3ac56b7cce997db768140d808efde684a
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-27 09:16:33 -0300

    add - informações

--- calculadora_simples.js
+++ calculadora_simples.js
@@ -1,9 +1,11 @@
 function somar(){
- let valor1 = Number(document.getElementById("numero1").value);
+ let valor1 = Number(document.getElementById("numero1").value); 
  let valor2 = Number(document.getElementById("numero2").value);
+ //Number(faz com que aceite apenas numeros) document(html) getelementbyid(procura no html o id) .value (uma variavel que recebe o que o input receber de informação)
  let resultado = valor1 + valor2;
 
 document.getElementById("resultado").innerText = "RESULTADO: " + resultado;
+//innertext(“Troca o texto que está dentro desse elemento pelo que eu mandei”)
 }
 
 function subtrair(){
