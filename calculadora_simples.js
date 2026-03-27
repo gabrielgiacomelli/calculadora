986a7d0a61f47dc9bd0e1cddbeb6667eaa2ae429
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-27 09:07:41 -0300

    up - function divison

--- calculadora_simples.js
+++ calculadora_simples.js
@@ -25,7 +25,13 @@ function multiplicar(){
 function dividir(){
     let valor1 = Number(document.getElementById("numero1").value);
     let valor2 = Number(document.getElementById("numero2").value);
-    let resultado = valor1 / valor2;
+    let resultado;
+
+    if (valor2 === 0){
+        resultado = "Não pode dividir por zero!";
+    } else {
+        resultado = valor1 / valor2;
+    }
 
     document.getElementById("resultado").innerText = "RESULTADO: " + resultado;
 }
