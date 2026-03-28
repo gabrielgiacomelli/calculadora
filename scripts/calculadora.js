745c1e7c2f71cde6510a15004d904f04695c13c6
gabrieldacostagiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-03-28 15:51:46 -0300

    add - funtion

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,4 +1,25 @@
 function operacao(){
     let texto = document.getElementById("valor1").value;
-    let conta = texto.split("");
+    let tudo = texto.split(/([+\-*/])/).map(item => item.trim());
+    let resultado;
+
+    const n1 = Number(tudo[0]);
+    const operador = tudo[1];
+    const n2 = Number(tudo[2]);
+
+    if (operador === "+") {
+      resultado = n1 + n2;
+      document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else if (operador === "-"){
+        resultado = n1 - n2;
+        document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else if (operador === "*") {
+        resultado = n1 * n2;
+        document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else if (operador === "/"){
+        resultado = n1 / n2;
+        document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else {
+        document.getElementById("resultado").innerText = "Resultado: Inválido";
+    }
 }
